49177b855d8950a818a7a1a33f39542be8feba3e
falsandtru <falsandtru@users.noreply.github.com>
2021-03-10 06:03:17 +0900

    Refactoring

--- test/interface/package.ts
+++ test/interface/package.ts
@@ -1,18 +1,23 @@
 import { IxList, MultiMap } from '../../index';
 
 describe('Interface: Package', function () {
+  describe('global', function () {
+    it('global', function () {
+      assert(global['IxList'] !== IxList);
+      assert(global['MultiMap'] !== MultiMap);
+    });
+  });
+
   describe('IxList', function () {
     it('IxList', function () {
       assert(typeof IxList === 'function');
     });
-
   });
 
   describe('MultiMap', function () {
     it('MultiMap', function () {
       assert(typeof MultiMap === 'function');
     });
-
   });
 
 });
